e11b8ab6599232e6a212343aeaaf5d638e6c13cf
include-yy <969041171@qq.com>
2026-03-01 17:19:53 +0800

    Massive improvements.

--- test/test.cpp
+++ test/test.cpp
@@ -2,33 +2,37 @@
 #include <iostream>
 #include <string>
 
-jsonrpc::Conn server;
+std::unique_ptr<jsonrpc::Conn> server;
 
 int main() {
-    // register methods
-    server.register_method("add", [](const jsonrpc::json& params) -> jsonrpc::json {
-        if (!params.is_array() || params.size() < 2) {
-            throw std::invalid_argument("Expected array with 2 numbers");
-        }
-        return params[0].get<double>() + params[1].get<double>();
-        });
-    server.register_method("echo", [](const jsonrpc::json& params) -> jsonrpc::json {
-        return params;
-        });
-    // start server
-    std::cerr << "[Server] Starting..." << std::endl;
-    server.start();
-
-    // Windows message loop
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        if (msg.message == WM_JSONRPC_MESSAGE) {
-            server.process_queue();
-        }
-        else {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
     return 0;
 }
+
+//int main() {
+//    // register methods
+//    server.register_method("add", [](const jsonrpc::json& params) -> jsonrpc::json {
+//        if (!params.is_array() || params.size() < 2) {
+//            throw std::invalid_argument("Expected array with 2 numbers");
+//        }
+//        return params[0].get<double>() + params[1].get<double>();
+//        });
+//    server.register_method("echo", [](const jsonrpc::json& params) -> jsonrpc::json {
+//        return params;
+//        });
+//    // start server
+//    std::cerr << "[Server] Starting..." << std::endl;
+//    server.start();
+//
+//    // Windows message loop
+//    MSG msg;
+//    while (GetMessage(&msg, NULL, 0, 0)) {
+//        if (msg.message == WM_JSONRPC_MESSAGE) {
+//            server.process_queue();
+//        }
+//        else {
+//            TranslateMessage(&msg);
+//            DispatchMessage(&msg);
+//        }
+//    }
+//    return 0;
+//}
